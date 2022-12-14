7a8002a2b3b41245d0c95a5f367b9c0f288ebb5e
Trang Nguyen <ntttrang2130@gmail.com>
2022-12-13 21:02:17 -0500

    add paddle

--- Day 22/paddle.py
+++ Day 22/paddle.py
@@ -1,16 +1,15 @@
 from turtle import Turtle
 
-
 class Paddle(Turtle):
-
-    def __init__(self, position):
+    def __init__(self):
         super().__init__()
+        # paddle = Turtle()
+        self.shape("square")
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
-
 
     def go_up(self):
         new_y = self.ycor() + 20
@@ -20,3 +19,14 @@ class Paddle(Turtle):
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
+
+# class Paddle(Turtle):
+
+#     def __init__(self, position):
+#         super().__init__()
+#         self.shape("square")
+#         self.color("white")
+#         self.shapesize(stretch_wid=5, stretch_len=1)
+#         self.penup()
+#         self.goto(position)
+
